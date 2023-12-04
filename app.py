613f66f54cd28a15636c963545942597d69fb328
AmrithaP <52408359+AmrithaP@users.noreply.github.com>
2023-12-04 02:44:49 -0500

    updates

--- app.py
+++ app.py
@@ -19,12 +19,18 @@ st.subheader("Input your text")
 
 text_input = st.text_input("Enter your text")
 
-if text_input is not None:
-    if st.button("Analyse"):
-        vect1 = load_tfidf()
-        text_tfidf = vect1.transform([text_input]).toarray()
-        nb_model = load_model()
-        prediction = nb_model.predict(text_tfidf)
-        result = "Toxic" if prediction == 1 else "Non-Toxic"
-        st.subheader("Result:")
-        st.info("The result is "+ result + ".")
+# Disable the button if the text_input is empty
+button_disabled = not text_input
+
+# Create a button with the conditional disabled attribute
+button_clicked = st.button("Analyse", disabled=button_disabled)
+
+if button_clicked and text_input:
+
+    vect1 = load_tfidf()
+    text_tfidf = vect1.transform([text_input]).toarray()
+    nb_model = load_model()
+    prediction = nb_model.predict(text_tfidf)
+    result = "Toxic" if prediction == 1 else "Non-Toxic"
+    st.subheader("Result:")
+    st.info("The result is "+ result + ".")
